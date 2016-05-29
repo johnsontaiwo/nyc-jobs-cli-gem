03e05a079d575b7557ddc71265edd1444eb05316
johnsontaiwo <josseytee@gmail.com>
2016-05-29 19:59:34 -0400

    scraped job-categories

--- lib/nyc_jobs/jobs.rb
+++ lib/nyc_jobs/jobs.rb
@@ -1,25 +1,38 @@
 class NycJobs::Jobs
 
-attr_accessor :date, :Business_title, :id, :location, :agency
+attr_accessor :date, :business_title, :job_id, :location, :agency, :category
  
- @@all = []
+ 
 
- def initialize(date = nil, id = nil, location = nil, agency = nil )
-  @date = date
-  @id = id 
-  @location = location
-  @agency = agency
-  @@all << self
+ def initialize(category = nil, url = nil)
+   @category = category
+   @url = url
+   
  end
- 
+  
+  def self.all
+    @@all ||= scrape_nyc_jobs
+    
+  end
+
+ def categories(category)
+  @category
+ end
+
  def self.scrape_nyc_jobs
   doc = Nokogiri::HTML(open("http://www1.nyc.gov/jobs"))
   
- end
-
-  def self.list_jobs_by_category
-    doc = Nokogiri::HTML(open("https://a127-jobs.nyc.gov/index_new.html?category=CAS"))
-     binding.pry
+  categories = doc.search("div.module-content .jobsbutton")
+  
+  jobs_categories = categories.collect{|b| new( b.text.gsub("/n", "").strip, "#{b.search("a").attribute("href").value}" )}
+  
+  
   end
-
+  
+  def self.list_jobs_categories
+    NycJobs::Jobs.all.collect.with_index(1) {|category, i| puts "#{i}. #{category.category}"}
+  end
+     
 end
+    
+
